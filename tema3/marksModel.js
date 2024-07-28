78565b4d23d21315e45f7543519ebe44ae34fd9e
JuditAF <idryscreep@gmail.com>
2024-07-28 21:56:33 +0200

    subo archivo con esquemas

--- tema3/marksModel.js
+++ tema3/marksModel.js
@@ -1,6 +1,21 @@
-const mongoose = require ("mongoose");
-const Marks = require ("./");
+const mongoose = require("mongoose");
+const Marks = require("./marks");
 
-mongoose.connect('mongodb://localhost:27017/t3', 
-    { useNewUrlParser: true, useUnifiedTopology: true }
-);
+mongoose.connect("mongodb://localhost:27017/codenotch", {                                   // PARA EVITAR PROBLEMAS USAR LA DIRECCIÓN 127.0.0.1
+    useNewUrlParser: false,
+    useUnifiedTopology: false,
+});
+
+let mark1 = new Marks({
+    date: ("2024-03-10"),
+    mark: 6,
+    student_first_name: "Juan",
+    student_last_name: "Torres Cuenca",
+    group_name: "D",
+    subject_name: "Dibujo Técnico",
+    teachers: [{
+        teacher_first_name: "Luis",
+        teacher_last_name: "Cortés Mesa"
+    }
+    ]
+});
